da51bb3ebd63e423430ef8d7a0ae13820ac0b8f8
steve.azzolin1@gmail.com <steve.azzolin1@gmail.com>
2019-08-06 16:22:59 +0200

    RicetteDao
    Completati e testati

--- src/main/java/it/unitn/disi/azzoiln_carretta_destro/persistence/dao/jdbc/JDBCRicettaDao.java
+++ src/main/java/it/unitn/disi/azzoiln_carretta_destro/persistence/dao/jdbc/JDBCRicettaDao.java
@@ -13,6 +13,7 @@ import it.unitn.disi.azzoiln_carretta_destro.persistence.entities.Utente;
 import it.unitn.disi.azzoiln_carretta_destro.utility.Common;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -24,22 +25,25 @@ import java.util.List;
  * @author Steve
  */
 public class JDBCRicettaDao extends JDBCDao<Ricetta,Integer> implements RicettaDao{
+    
+    public JDBCRicettaDao(Connection con){
+        super(con);
+    }
 
     @Override
     public List<Ricetta> getRicette(Integer id_paziente) throws DaoException {
         List<Ricetta> ret = new LinkedList<>();
         
-        try (PreparedStatement stm = CON.prepareStatement("SELECT r.*, f.nome FROM ricette r inner join farmaci f on f.id = r.id_farmaco WHERE id_paziente = ?")) {
+        try (PreparedStatement stm = CON.prepareStatement("SELECT r.*,f.nome,p.* FROM farmaco r inner join farmaci f on f.id = r.id_farmaco inner join prescrizione p on p.id = r.id_prescrizione WHERE id_prescrizione = ?")) {
             stm.setInt(1, id_paziente);
             ResultSet rs = stm.executeQuery();
             while (rs.next()) {
-                 Ricetta r = new Ricetta(rs.getInt("id_prescrizione"),rs.getInt("id_paziente"),rs.getInt("id_medico"),rs.getInt("id_farmaco"),rs.getString("nome_farmaco"), rs.getFloat("costo"),rs.getShort("quantita"), rs.getDate("time_vendita"), rs.getDate("time_prescrizione"));
+                 Ricetta r = new Ricetta(rs.getInt("id_prescrizione"),rs.getInt("id_paziente"),rs.getInt("id_medico"),rs.getInt("id_farmaco"),rs.getString("nome"), rs.getFloat("costo"),rs.getShort("quantita"), rs.getDate("time_vendita"), rs.getDate("time"));
                  ret.add(r);
             }            
         } catch (SQLException ex) {
             throw new DaoException("Error retriving List<Ricetta>", ex);
-        }
-        
+        }        
         return ret;
     }
 
@@ -47,16 +51,15 @@ public class JDBCRicettaDao extends JDBCDao<Ricetta,Integer> implements RicettaD
     public Ricetta getByPrimaryKey(Integer id_ricetta) throws DaoException {
         Ricetta ret = null;
         
-        try (PreparedStatement stm = CON.prepareStatement("SELECT r.*, f.nome FROM ricette r inner join farmaci f on f.id = r.id_farmaco WHERE id_prescrizione = ?")) {
+        try (PreparedStatement stm = CON.prepareStatement("SELECT r.*,f.nome,p.* FROM farmaco r inner join farmaci f on f.id = r.id_farmaco inner join prescrizione p on p.id = r.id_prescrizione WHERE id_prescrizione = ?")) {
             stm.setInt(1, id_ricetta);
             ResultSet rs = stm.executeQuery();
             if (rs.next()) {
-                 Ricetta r = new Ricetta(rs.getInt("id_prescrizione"),rs.getInt("id_paziente"),rs.getInt("id_farmaco"), rs.getString("nome_farmaco"), rs.getDate("time_prescrizione"));
+                 ret = new Ricetta(rs.getInt("p.id"),rs.getInt("id_paziente"),rs.getInt("id_medico"),rs.getInt("id_farmaco"),rs.getString("nome"), rs.getFloat("costo"),rs.getShort("quantita"), rs.getDate("time_vendita"), rs.getDate("time"));
             }            
         } catch (SQLException ex) {
             throw new DaoException("Error retriving Ricetta", ex);
-        }
-        
+        }        
         return ret;
     }
 
@@ -78,13 +81,16 @@ public class JDBCRicettaDao extends JDBCDao<Ricetta,Integer> implements RicettaD
             if(count == 0) return false;
             
             ps = CON.prepareStatement("SELECT id FROM prescrizione WHERE id_paziente = ? AND id_medico= ? ORDER BY id DESC LIMIT 1");  //questo perchè MYSQL non supporta la sintassi RETURNING
+            ps.setInt(1, r.getId_paziente());
+            ps.setInt(2, r.getId_medico());
+            
             ResultSet rs = ps.executeQuery();
             if(rs.next()){
                 int id_new_ricetta = rs.getInt("id");
                 ps = CON.prepareStatement("insert into farmaco (id_prescrizione, id_farmaco, quantita) VALUES (?,?,?)");
                 ps.setInt(1, id_new_ricetta);
-                ps.setInt(1, r.getId_farmaco());
-                ps.setInt(1, r.getQuantita());
+                ps.setInt(2, r.getId_farmaco());
+                ps.setInt(3, r.getQuantita());
                 count = ps.executeUpdate();
                 if(count == 0) return false;
             }
