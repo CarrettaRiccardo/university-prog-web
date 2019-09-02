f85ef5cda3ab7217ec1cfa9ec5ec6f7ced983989
steve.azzolin1@gmail.com <steve.azzolin1@gmail.com>
2019-09-02 22:14:40 +0200

    Finito STATS per SSP

--- src/main/java/it/unitn/disi/azzoiln_carretta_destro/persistence/dao/jdbc/JDBCSspDao.java
+++ src/main/java/it/unitn/disi/azzoiln_carretta_destro/persistence/dao/jdbc/JDBCSspDao.java
@@ -7,16 +7,19 @@ package it.unitn.disi.azzoiln_carretta_destro.persistence.dao.jdbc;
 
 import it.unitn.disi.azzoiln_carretta_destro.persistence.dao.SspDao;
 import it.unitn.disi.azzoiln_carretta_destro.persistence.dao.external.exceptions.DaoException;
+import it.unitn.disi.azzoiln_carretta_destro.persistence.dao.external.exceptions.IdNotFoundException;
 import it.unitn.disi.azzoiln_carretta_destro.persistence.dao.external.jdbc.JDBCDao;
 import it.unitn.disi.azzoiln_carretta_destro.persistence.entities.Medico;
 import it.unitn.disi.azzoiln_carretta_destro.persistence.entities.MedicoSpecialista;
 import it.unitn.disi.azzoiln_carretta_destro.persistence.entities.Paziente;
 import it.unitn.disi.azzoiln_carretta_destro.persistence.entities.Ssp;
+import it.unitn.disi.azzoiln_carretta_destro.persistence.wrappers.Statistiche;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -136,5 +139,60 @@ public class JDBCSspDao extends JDBCDao<Ssp, Integer> implements SspDao {
             throw new DaoException("db_error", ex);
         }
         return ret;
+    }
+
+    @Override
+    public ArrayList<ArrayList<Integer>> getStatsEsami(int id_ssp) throws DaoException {
+        if(id_ssp <= 0) throw new IdNotFoundException("id_ssp");
+        ArrayList< ArrayList<Integer> > ret2 = new ArrayList<>();
+        
+        try (PreparedStatement stm = CON.prepareStatement(  "SELECT COUNT(p.id) as tot, seq.mese, seq.anno\n" +
+                                                            "FROM \n" +
+                                                            "     (\n" +
+                                                            "      SELECT * FROM\n" +
+                                                            "        (\n" +
+                                                            "			SELECT 1 AS mese UNION SELECT 2  UNION SELECT 3 UNION SELECT 4 UNION\n" +
+                                                            "			SELECT 5 UNION SELECT 6  UNION SELECT 7 UNION SELECT 8 UNION \n" +
+                                                            "			SELECT 9 UNION SELECT 10 UNION SELECT 11 UNION SELECT 12\n" +
+                                                            "		) as tmp , (SELECT DISTINCT YEAR(time_visita) as anno FROM esame WHERE id_ssp = ? AND YEAR(time_visita) >= YEAR(NOW()) - 2) as tmp2\n" +
+                                                            "      ) AS seq \n" +
+                                                            "LEFT JOIN (prescrizione p  inner join esame f ON f.id_ssp = ? AND p.id = f.id_prescrizione) ON seq.mese = MONTH(time_visita) AND seq.anno = YEAR(time_visita)\n" +
+                                                            "WHERE YEAR(time_visita) >= YEAR(NOW()) - 2 OR time IS NULL\n" +
+                                                            "GROUP BY seq.anno, seq.mese\n" +
+                                                            "ORDER BY seq.anno, seq.mese")) {
+            stm.setInt(1, id_ssp);
+            stm.setInt(2, id_ssp);
+            ResultSet rs = stm.executeQuery();
+            for (int i = 0; i < 12; i++) {
+                ret2.add(new ArrayList<Integer>());
+            }
+            
+            Statistiche m = new Statistiche();
+            while (rs.next()) {
+                 System.out.println(rs.getInt("tot"));
+                 Statistiche.LightStats s = m.new LightStats(rs.getInt("tot"), rs.getInt("mese"), rs.getInt("anno"));
+                 ret2.get(s.mese-1).add(s.count);                    
+            }            
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+            throw new DaoException("db_error", ex);
+        }    
+        if(ret2.get(0).size() == 0){ //se non c'è nessun elemento meto degli 0 per evitare problemi nel forEach in JSP
+            for (int i = 0; i < 12; i++) {
+                ret2.get(i).add(0);
+            }
+        }
+        
+        System.out.println("STAMPO STATS_ESAME");
+        int i = 0;
+        for(ArrayList<Integer> m : ret2){
+            System.out.print("Mese:" + i);
+            for(Integer m2 : m){
+                System.out.print(" -> " + m2);
+            }
+            System.out.println("");
+            i++;
+        }
+        return ret2;
     }
 }
